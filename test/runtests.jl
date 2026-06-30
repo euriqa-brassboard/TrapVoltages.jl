db0848596ab33864e239107ce6a58bcee3c8f576
Yichao Yu <yyc1992@gmail.com>
2026-06-30 14:59:48 -0400

    Add distributed test runner

---
+++ test/runtests.jl
@@ -0,0 +1,23 @@
+#!/usr/bin/julia
+
+using Distributed
+
+env_nproc = get(ENV, "TRAP_VOLTAGES_NPROC", nothing)
+if env_nproc === nothing
+    addprocs(Sys.CPU_THREADS)
+else
+    env_nproc = parse(Int, env_nproc)
+    if env_nproc > 0
+        addprocs(env_nproc)
+    end
+end
+
+pmap([]) do file
+    println("Start testing $file")
+    @eval module $(Symbol("Test_$(file)_mod"))
+    include($(joinpath(@__DIR__, "$(file).jl")))
+    end
+    println("Done testing $file")
+    # So that we do not try to bring the worker-only module back to the main process
+    return
+end
