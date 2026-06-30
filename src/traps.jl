b05beddd2afd19cde4838c1df28f9bd563bc4526
Yichao Yu <yyc1992@gmail.com>
2026-06-30 17:53:35 -0400

    Store trap name in TrapDesc

--- src/traps.jl
+++ src/traps.jl
@@ -5,23 +5,29 @@ module Traps
 export TrapDesc
 
 struct TrapDesc
+    name::String
     ele_names::Vector{String}
     ele_indices::Dict{String,Int}
-    function TrapDesc(ele_names)
+    function TrapDesc(name, ele_names)
         ele_indices = Dict{String,Int}()
-        for (i, name) in enumerate(ele_names)
-            ele_indices[name] = i
+        for (i, ele_name) in enumerate(ele_names)
+            ele_indices[ele_name] = i
         end
-        return new(ele_names, ele_indices)
+        return new(name, ele_names, ele_indices)
     end
 end
 
-const _trap_px = TrapDesc(["GND"; "RF";
-                           "L" .* string.(0:9);
-                           "O" .* string.(0:1);
-                           "Q" .* string.(0:65);
-                           "S" .* string.(0:11);])
-const _trap_hoa = TrapDesc(["GND"; "RF";
+const _trap_phoenix = TrapDesc("phoenix", ["GND"; "RF";
+                                "L" .* string.(0:9);
+                                "O" .* string.(0:1);
+                                "Q" .* string.(0:65);
+                                "S" .* string.(0:11);])
+const _trap_peregrine = TrapDesc("peregrine", ["GND"; "RF";
+                                  "L" .* string.(0:9);
+                                  "O" .* string.(0:1);
+                                  "Q" .* string.(0:65);
+                                  "S" .* string.(0:11);])
+const _trap_hoa = TrapDesc("hoa", ["GND"; "RF";
                             "G" .* string.(1:8);
                             "L" .* string.(1:16);
                             "Q" .* string.(1:40);
@@ -30,8 +36,10 @@ const _trap_hoa = TrapDesc(["GND"; "RF";
 
 TrapDesc(trap::TrapDesc) = trap
 function TrapDesc(name::AbstractString)
-    if name == "phoenix" || name == "peregrine"
-        return _trap_px
+    if name == "phoenix"
+        return _trap_phoenix
+    elseif name == "peregrine"
+        return _trap_peregrine
     elseif name == "hoa"
         return _trap_hoa
     end
