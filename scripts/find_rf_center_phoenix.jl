4f08601830fb54e544f600fc73e6cc1befa1db2d
Yichao Yu <yyc1992@gmail.com>
2026-07-01 17:25:28 -0400

    Include RF center data

---
+++ scripts/find_rf_center_phoenix.jl
@@ -0,0 +1,30 @@
+#!/usr/bin/julia
+
+using TrapVoltages: Potentials, Solutions
+
+using HDF5
+
+# This should be pointing to the RS1394_coarse.va file (without extension).
+const potential_prefix = ARGS[1]
+const data_prefix = joinpath(@__DIR__, "../data/phoenix/rf_center")
+
+function compute_center(file, trap)
+    potential = Potentials.import_pillbox_64(file, trap=trap)
+    # RF is electrode 2 (ground is 1)
+    centers = Solutions.find_all_flat_points(@view(potential.data[:, :, :, 2]))
+    centers_um = similar(centers)
+    for i in 1:potential.nx
+        centers_um[i, 1] = Potentials.z_index_to_axis(potential, centers[i, 1]) * 1000
+        centers_um[i, 2] = Potentials.y_index_to_axis(potential, centers[i, 2]) * 1000
+    end
+    return centers, centers_um
+end
+
+mkpath(dirname(data_prefix))
+
+h5open("$(data_prefix).h5", "w") do fh
+    g = create_group(fh, "1") # region 1
+    centers, centers_um = compute_center("$(potential_prefix).va", "phoenix")
+    g["zy_index"] = centers
+    g["zy_um"] = centers_um
+end
