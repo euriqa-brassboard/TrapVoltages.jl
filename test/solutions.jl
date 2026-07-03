5e111496a0f8aaf9e02365408a2b597a823d7e06
Yichao Yu <yyc1992@gmail.com>
2026-07-03 18:02:54 -0400

    Test compensation solution generation

--- test/solutions.jl
+++ test/solutions.jl
@@ -1,10 +1,11 @@
 #
 
-using TrapVoltages: Solutions, CenterTracker, PolyFit as PF
+using TrapVoltages: Solutions, CenterTracker, PolyFit as PF, TrapDesc, Potentials, Traps
 using TrapVoltages.Units: TrapUnits
 
 using Test
 using HDF5
+using LinearAlgebra
 
 @testset "find_flat_point" begin
     xs = 1:10
@@ -96,7 +97,7 @@ unwrap(::Val{V}) where V = V
                                                (false, true))
         mask = Solutions.TermMask(dx=dx, dy=dy, dz=dz, xy=xy, yz=yz, zx=zx, z2=z2,
                                   x2=x2, x3=x3, x4=x4, x2z=x2z)
-        nterms =  dx + dy + dz + xy + yz + zx + z2 + x2 + x3 + x4 + x2z
+        nterms = dx + dy + dz + xy + yz + zx + z2 + x2 + x3 + x4 + x2z
 
         @test count(unwrap(mask)) == nterms
         if 3 < nterms < 9
@@ -159,6 +160,127 @@ unwrap(::Val{V}) where V = V
             @test_term x4
 
             @test_term x2z
+        end
+    end
+end
+
+@testset "Compensation Terms Fit" begin
+    trap = TrapDesc("phoenix")
+    origin = (-500, -4, -4)
+    p = Potentials.Potential(length(trap.ele_names), 1001, 9, 9, (1, 1, 1), origin,
+                             PermutedDimsArray(zeros(9, 9, 1001, length(trap.ele_names)),
+                                               (3, 2, 1, 4)),
+                             trap.ele_indices, [[n] for n in trap.ele_names], trap)
+    unit = TrapUnits((rand() + 0.5) * 1e-6, rand() + 0.5)
+    eles = sort!(collect(Traps.find_electrodes(trap, trap.ele_indices, 0, min_num=20)))
+    neles = length(eles)
+    @test neles >= 20
+
+    for (dx, dy, dz, xy, yz, zx, z2,
+         x2, x3, x4, x2z) in Iterators.product((false, true), (false, true),
+                                               (false, true), (false, true),
+                                               (false, true), (false, true),
+                                               (false, true), (false, true),
+                                               (false, true), (false, true),
+                                               (false, true))
+        mask = Solutions.TermMask(dx=dx, dy=dy, dz=dz, xy=xy, yz=yz, zx=zx, z2=z2,
+                                  x2=x2, x3=x3, x4=x4, x2z=x2z)
+        nterms = dx + dy + dz + xy + yz + zx + z2 + x2 + x3 + x4 + x2z
+
+        @test count(unwrap(mask)) == nterms
+        if 2 < nterms < 10 || nterms == 0
+            # Limit compilation
+            continue
+        end
+
+        function fill_potential!(vs, eid)
+            dx_c, dy_c, dz_c, xy_c, yz_c, zx_c, z2_c, x2_c, x3_c, x4_c, x2z_c = vs
+            x2y2z2_c = rand()
+            real_x2 = x2y2z2_c + x2_c / 2
+            real_y2 = x2y2z2_c - x2_c / 4 - z2_c / 2
+            real_z2 = x2y2z2_c - x2_c / 4 + z2_c / 2
+
+            x1_c = dx_c / 1e6
+            y1_c = dy_c / 1e6
+            z1_c = dz_c / 1e6
+
+            xy_c = xy_c * unit.V_unit / unit.l_unit_um^2
+            yz_c = yz_c * unit.V_unit / unit.l_unit_um^2
+            zx_c = zx_c * unit.V_unit / unit.l_unit_um^2
+
+            x2_c = real_x2 * unit.V_unit / unit.l_unit_um^2
+            y2_c = real_y2 * unit.V_unit / unit.l_unit_um^2
+            z2_c = real_z2 * unit.V_unit / unit.l_unit_um^2
+
+            x3_c = x3_c / 6 * unit.V_unit / unit.l_unit_um^3
+            x4_c = x4_c / 24 * unit.V_unit / unit.l_unit_um^4
+            x2z_c = x2z_c / 2 * unit.V_unit / unit.l_unit_um^3
+
+            for x in 1:p.nx
+                xv = Potentials.x_index_to_axis(p, x)
+                x2v = xv^2
+                x3v = xv^3
+                x4v = xv^4
+                for y in 1:p.ny
+                    yv = Potentials.y_index_to_axis(p, y)
+                    y2v = yv^2
+                    xyv = xv * yv
+                    for z in 1:p.nz
+                        zv = Potentials.z_index_to_axis(p, z)
+                        z2v = zv^2
+                        yzv = yv * zv
+                        zxv = zv * xv
+                        x2zv = x2v * zv
+                        p.data[x, y, z, eid] = (x1_c * xv + y1_c * yv + z1_c * zv +
+                            xy_c * xyv + yz_c * yzv + zx_c * zxv + x2_c * x2v +
+                            y2_c * y2v + z2_c * z2v + x3_c * x3v + x4_c * x4v +
+                            x2z_c * x2zv)
+                    end
+                end
+            end
+        end
+
+        center_idx = (Potentials.x_axis_to_index(p, 0),
+                      Potentials.y_axis_to_index(p, 0),
+                      Potentials.z_axis_to_index(p, 0))
+
+        for _ in 1:5
+            coeffs = rand(11, neles)
+            for (i, eid) in enumerate(eles)
+                fill_potential!(coeffs[:, i], eid)
+            end
+            fitting = Potentials.Fitting(p, orders=(4, 2, 2), sizes=(41, 5, 5))
+            eles2, coeffs2 = Solutions.compensate_terms(fitting, center_idx;
+                                                        unit=unit, mask=mask, min_num=20)
+            @test eles2 == eles
+
+            @test coeffs2 ≈ coeffs[[dx, dy, dz, xy, yz, zx, z2, x2, x3, x4, x2z], :] atol=1e-4 rtol=1e-4
+
+            eles3, terms3 = Solutions.solve_compensate(fitting, center_idx;
+                                                       unit=unit, mask=mask,
+                                                       min_num=20, minmax=true)
+            @test eles3 == eles
+            @test coeffs2 * hcat(terms3...) ≈ Matrix(I, nterms, nterms) atol=1e-6 rtol=1e-6
+
+            eles4, terms4 = Solutions.solve_compensate(fitting, center_idx;
+                                                       unit=unit, mask=mask,
+                                                       min_num=20, minmax=false)
+            @test eles4 == eles
+            @test coeffs2 * hcat(terms4...) ≈ Matrix(I, nterms, nterms) atol=1e-6 rtol=1e-6
+
+            tgt5 = rand(nterms)
+            eles5, terms5 = Solutions.solve_target(fitting, center_idx, tgt5;
+                                                  unit=unit, mask=mask,
+                                                  min_num=20, minmax=true)
+            @test eles5 == eles
+            @test coeffs2 * terms5 ≈ tgt5 atol=1e-6 rtol=1e-6
+
+            tgt6 = rand(nterms)
+            eles6, terms6 = Solutions.solve_target(fitting, center_idx, tgt6;
+                                                  unit=unit, mask=mask,
+                                                  min_num=20, minmax=false)
+            @test eles6 == eles
+            @test coeffs2 * terms6 ≈ tgt6 atol=1e-6 rtol=1e-6
         end
     end
 end
